41472495889a606941dc6eb290615d7f57684d85
melekakballl@gmail.com <melekakballl@gmail.com>
2022-07-09 13:09:20 +0300

    SQL Bağlanti sınıfı oluştu

--- Kitaplık_Projesi/Form1.cs
+++ Kitaplık_Projesi/Form1.cs
@@ -16,5 +16,10 @@ namespace Kitaplık_Projesi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Kitaplık_Projesi/Sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Kitaplık_Projesi
+{
+    internal class Sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-D0LP1EG;Initial Catalog=Kitaplık;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
